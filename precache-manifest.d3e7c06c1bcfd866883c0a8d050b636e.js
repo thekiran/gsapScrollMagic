f2cade982c91d874bfe340d9e810368bde80de71
unknown <kirankumargs23.kkgs@gmail.com>
2021-02-02 17:27:36 +0530

    Updates

---
+++ precache-manifest.d3e7c06c1bcfd866883c0a8d050b636e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "153fe857379a607c8156",
+    "url": "/static/css/main.e9e4eeda.chunk.css"
+  },
+  {
+    "revision": "153fe857379a607c8156",
+    "url": "/static/js/main.e1636404.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "138280100d72e5dc2c13",
+    "url": "/static/js/2.9e7a66d0.chunk.js"
+  },
+  {
+    "revision": "3646fe337a9cbf8a717f4fa8c1cedb06",
+    "url": "/index.html"
+  }
+];
